759786b2b7c6dda8a54ab3768af56755c8a77577
joey.ren <xiaoming_1862@163.com>
2024-07-03 15:49:27 +0800

    update

--- pub_finance/finance/checkin.sh
+++ pub_finance/finance/checkin.sh
@@ -1,4 +1,4 @@
-\cp -rf finance ./git-repo/pub_finance
+\cp -rf finance ./git-repo/pub_finance/
 rm -f ./git-repo/pub_finance/finance/us.log
 rm -f ./git-repo/pub_finance/finance/cn.log
 rm -f ./git-repo/pub_finance/finance/cnetf.log
@@ -7,10 +7,11 @@ rm -rf ./git-repo/pub_finance/finance/usstockinfo/*
 rm -rf ./git-repo/pub_finance/finance/cnstockinfo/*
 rm -rf ./git-repo/pub_finance/finance/ta-lib-0.4.0-src.tar.gz
 rm -rf ./git-repo/pub_finance/finance/ta-lib
+rm -rf ./git-repo/pub_finance/finance/shanghaidistrict.json
 echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/finance/mail.conf
 \cp -rf ./finance/usstockinfo/USStockMarketClosed.config ./git-repo/pub_finance/finance/usstockinfo/
 \cp -rf ./finance/cnstockinfo/CNStockMarketClosed.config ./git-repo/pub_finance/finance/cnstockinfo/
-cd ./git-repo/pub_finance
-git add finance
-git commit -m "update"
+cd ./git-repo/pub_finance/
+git add *
+git commit -a -m "update"
 git push
