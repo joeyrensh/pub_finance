6de0d62736a09af7a422550a2068b1b9b2ac0b26
joey.ren <xiaoming_1862@163.com>
2024-07-17 19:01:14 +0800

    update

--- pub_finance/finance/checkin.sh
+++ pub_finance/finance/checkin.sh
@@ -5,6 +5,7 @@ rm -rf ./git-repo/pub_finance/finance/cnstockinfo/*
 rm -rf ./git-repo/pub_finance/finance/ta-lib-0.4.0-src.tar.gz
 rm -rf ./git-repo/pub_finance/finance/ta-lib
 rm -rf ./git-repo/pub_finance/finance/houseinfo/*.png
+rm -rf ./git-repo/pub_finance/finance/*.png
 rm -rf ./git-repo/pub_finance/finance/houseinfo/*.csv
 echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/finance/mail.conf
 \cp -rf ./finance/usstockinfo/USStockMarketClosed.config ./git-repo/pub_finance/finance/usstockinfo/
