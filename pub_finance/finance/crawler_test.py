3ee0b71d3fe4b2c94eafdd9b813f474b2f0cab1e
joey.ren <xiaoming_1862@163.com>
2024-07-24 19:11:18 +0800

    update

--- pub_finance/finance/crawler_test.py
+++ pub_finance/finance/crawler_test.py
@@ -1,100 +1,119 @@
 import requests
-from lxml import html
-import pandas as pd
-from utility.ToolKit import ToolKit
-import concurrent.futures
 import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from utility.MyEmail import MyEmail
-import seaborn as sns
-from matplotlib.transforms import Bbox
+from utility.ToolKit import ToolKit
 import os
-import contextily as cx
+from lxml import html
+import concurrent.futures
 import re
-import numpy as np
 import json
-import matplotlib.colors as colors
-import sys
-import pysal.viz.mapclassify as mc
-import httpx
-import asyncio
-import aiohttp
-from fake_useragent import UserAgent
-from selenium import webdriver
+
+geo_path = "./houseinfo/shanghaidistrict.json"
+file_path_s = "./houseinfo/secondhandhouse.csv"
+geo_data = gpd.read_file(geo_path, engine="pyogrio")
+# 二手房数据分析
+df_second_hand_house = pd.read_csv(file_path_s, usecols=[i for i in range(0, 12)])
+df_second_hand_house = df_second_hand_house.dropna(subset=["lanlong"])
 
 
-def fetch_house_info(url, file_path):
-
-    # 添加请求头
-    ua = UserAgent()
-    headers =  {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-                'Content-Length': '0',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                'Accept': 'application/json, text/javascript, */*; q=0.01',
-                'Accept-Encoding': 'gzip, deflate',
-                'Connection': 'keep-alive',
-                'host': 'www.fangdi.com.cn',
-                'Origin': 'http://www.fangdi.com.cn',
-                'Referer': 'http://www.fangdi.com.cn/index.html',
-                'Upgrade-Insecure-Requests': '1',
-                'X-Requested-With': 'XMLHttpRequest',
-                'POST': '/service/index/getIndexMessage.action?MmEwMD=4CoFxR.BwKwk9w7O5h2NIAWhebrwn0Hc1HUNZpKZzuHrLuiO9N.e6Ou6hNuH57EWB1kL9CxOmcQVi68k0WuCkeRoXzjeQHoxydyAwZ5bnWaNAXigCAH8VyPxJAHKRKQ..WszMKrtDTJCie8gWGawxGdKdIDgDrcW3UBNN7h1BoczD29Jxqw1kcEJvWSDzAm4PVtRiNwCBuRWTlX.DNzJfwrGF8YaVtUB87WMTvpOr7poF.A0Vh_ZrU0OqdLSLG_hmwKAJoThMFjLLYknh0_yuRGzy8L6atTFhZ6q6iEahGx4d67jXscB7Mk1hGYYk_RcxsC91Im0Bp.vHY5yyTHYScRQuV..CX.OtfUi6jSoSx829wIZAg2R36NsRPscwREO1hNz HTTP/1.1'
-                }
-    data = {
-    }
-    session = session = requests.Session()
-    response = session.get(url, headers=headers, data=data)
+# 处理二手房经纬度格式
+def extract_values(string):
+    values = string.strip("[]").split(",")
+    value1 = float(values[0])
+    value2 = float(values[1])
+    return value1, value2
 
 
-    dict = {}
-    # response = requests.post(url, headers=headers)
-    print(response.status_code)  # 打印状态码
-    print(response.text)  # 打印响应内容
-    # tree = html.fromstring(response.content)
-    # first_supply_div = tree.xpath('//div[@class="content_box"]/div[@class="layout content"]/div[@class="house_supply"]/div[@class="main_wrapper clearfix"]/div[@class="main_item_left"]/div[@class="main_item_content house_supply_content grey_shadow"]/div[@class="house_supply_district house_supply_num"]/span2/i1')
-    # first_supply_div = tree.xpath('//div[@class="content_box"]/div[@class="layout content"]/div[@class="house_supply"]/div[@class="main_wrapper clearfix"]/div[@class="main_item_right"]/div[@class="today_sign grey_shadow"]/div[@class="sign_top1"]/p/span')
-    # first_supply = first_supply_div[0].xpath('string(.)')
- 
-async def fetch_data(url):
-    # 添加请求头
-    ua = UserAgent()
-    headers =  {'User-Agent':ua.random,
-                'Content-Type': 'application/json',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                'Connection': 'keep-alive',
-                'Referer': 'http://www.fangdi.com.cn/service/index/getIndexMessage',
-                'Origin': 'http://www.fangdi.com.cn',
-                'host': 'www.fangdi.com.cn',
-                'Upgrade-Insecure-Requests': '1'
-                }
-    
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers, timeout=10)
-        print(response.status_code)  # 打印状态码
-        print(response.headers)  # 打印响应内容
-        print(response.text)  # 打印响应内容
-async def main(url):
-    result = await fetch_data(url)
-    # print(result)
+df_second_hand_house[["longitude", "latitude"]] = (
+    df_second_hand_house["lanlong"].apply(extract_values).apply(pd.Series)
+)
 
-# 主程序入口
-if __name__ == "__main__":
-    
-    file_path = './houseinfo/test.csv'
-    # url='http://www.fangdi.com.cn/index.html'
-    url = 'http://www.fangdi.com.cn/service/index/getIndexMessage.action'
-    # fetch_house_info(url,file_path)
-    # 创建一个事件循环并运行异步函数
-    # asyncio.run(main(url))
-    import datetime, os
-    import pandas as pd
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium import webdriver
 
-    options = webdriver.FirefoxOptions()
-    # options.set_headless()      # 无头模式
-    driver = webdriver.Firefox()
-    result = driver.get(r'http://www.fangdi.com.cn/index.html') 
-    print(result)
+# 定义一个函数来替换中文字符
+def replace_non_numeric_characters(text, replacement=""):
+    # 使用正则表达式匹配非数字字符
+    numeric_text = re.sub(r"\D", "", str(text))
+    # 检查字符串是否为空
+    if numeric_text == "":
+        return None  # 返回空字符串
+    # 返回转换后的整数类型
+    return int(numeric_text)
+
+
+df_second_hand_house["total_cnt"] = df_second_hand_house["total_cnt"].apply(
+    replace_non_numeric_characters
+)
+df_second_hand_house["deal_price"] = df_second_hand_house["deal_price"].apply(
+    replace_non_numeric_characters
+)
+gdf_second_hand_house = gpd.GeoDataFrame(
+    df_second_hand_house,
+    geometry=gpd.points_from_xy(
+        df_second_hand_house["longitude"], df_second_hand_house["latitude"]
+    ),
+    crs="EPSG:4326",
+)
+# 处理板块级别数据
+exclude_values = [
+    310104,
+    310101,
+    310106,
+    310109,
+    310105,
+    310110,
+    310107,
+]  # 要排除的值的列表
+geo_data_s = geo_data[
+    ((geo_data.level == "district") & (geo_data.adcode.isin(exclude_values)))
+    | (
+        (geo_data.level == "town")
+        & (
+            ~geo_data["parent"]
+            .apply(lambda x: json.loads(x)["adcode"])
+            .isin(exclude_values)
+        )
+    )
+]
+gdf_merged = gpd.sjoin(
+    geo_data_s, gdf_second_hand_house, how="inner", predicate="intersects"
+)
+gdf_agg_unit_price = (
+    gdf_merged.groupby("adcode").median({"unit_price": "unit_price"}).round(-2)
+)
+gdf_agg_deal_price = (
+    gdf_merged.groupby("adcode").median({"deal_price": "deal_price"}).round(-2)
+)
+gdf_agg_sell = gdf_merged.groupby("adcode")[["sell_cnt", "total_cnt"]].sum().round(0)
+gdf_agg_sell["ratio"] = gdf_agg_sell.apply(
+    lambda row: row["sell_cnt"] / row["total_cnt"] if row["total_cnt"] != 0 else None,
+    axis=1,
+)
+result = geo_data_s.merge(
+    gdf_agg_unit_price, how="left", left_on="adcode", right_on="adcode"
+)
+result = result.merge(
+    gdf_agg_deal_price, how="left", left_on="adcode", right_on="adcode"
+)
+result = result.merge(gdf_agg_sell, how="left", left_on="adcode", right_on="adcode")
+result.drop(
+    columns=[
+        "adcode",
+        "childrenNum",
+        "childrenNum_x",
+        "parent",
+        "subFeatureIndex_x",
+        "geometry",
+        "childrenNum_y",
+        "subFeatureIndex_y",
+        "longitude_y",
+        "latitude_y",
+        "index_right_x",
+        "data_id_x",
+        "data_id_x",
+    ],
+    inplace=True,
+)
+print(result)
