e9c27da06c9dca0a6a7da8de57b448e8814cb4b9
joey <xiaoming_1862@163.com>
2024-07-31 16:37:14 +0800

    update

--- pub_finance/finance/checkin.sh
+++ pub_finance/finance/checkin.sh
@@ -1,16 +1,16 @@
-\cp -rf finance ./git-repo/pub_finance/
-rm -rf ./git-repo/pub_finance/finance/*.log
-rm -rf ./git-repo/pub_finance/finance/usstockinfo/*
-rm -rf ./git-repo/pub_finance/finance/cnstockinfo/*
-rm -rf ./git-repo/pub_finance/finance/ta-lib-0.4.0-src.tar.gz
-rm -rf ./git-repo/pub_finance/finance/ta-lib
-rm -rf ./git-repo/pub_finance/finance/houseinfo/*.png
-rm -rf ./git-repo/pub_finance/finance/*.png
-rm -rf ./git-repo/pub_finance/finance/houseinfo/*.csv
-echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/finance/mail.conf
-\cp -rf ./finance/usstockinfo/USStockMarketClosed.config ./git-repo/pub_finance/finance/usstockinfo/
-\cp -rf ./finance/cnstockinfo/CNStockMarketClosed.config ./git-repo/pub_finance/finance/cnstockinfo/
-cd ./git-repo/pub_finance/
+\cp -rf ./pub_finance/finance ./git-repo/pub_finance/pub_finance/
+rm -rf ./git-repo/pub_finance/pub_finance/finance/*.log
+rm -rf ./git-repo/pub_finance/pub_finance/finance/usstockinfo/*
+rm -rf ./git-repo/pub_finance/pub_finance/finance/cnstockinfo/*
+rm -rf ./git-repo/pub_finance/pub_finance/finance/ta-lib-0.4.0-src.tar.gz
+rm -rf ./git-repo/pub_finance/pub_finance/finance/ta-lib
+rm -rf ./git-repo/pub_finance/pub_finance/finance/houseinfo/*.png
+rm -rf ./git-repo/pub_finance/pub_finance/finance/*.png
+rm -rf ./git-repo/pub_finance/pub_finance/finance/houseinfo/*.csv
+echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/pub_finance/finance/mail.conf
+\cp -rf ./pub_finance/finance/usstockinfo/USStockMarketClosed.config ./git-repo/pub_finance/pub_finance/finance/usstockinfo/
+\cp -rf ./pub_finance/finance/cnstockinfo/CNStockMarketClosed.config ./git-repo/pub_finance/pub_finance/finance/cnstockinfo/
+cd ./git-repo/pub_finance/pub_finance/
 git add *
 git commit -a -m "update"
 git push
