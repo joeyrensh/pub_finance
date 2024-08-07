4370553259dc5c82c294f0424ca919b034790e50
joey <xiaoming_1862@163.com>
2024-08-07 18:29:57 +0800

    update

--- pub_finance/finance/dashreport/app.py
+++ pub_finance/finance/dashreport/app.py
@@ -22,7 +22,7 @@ app.layout = html.Div(
         dcc.Location(id="url", refresh=False),
         dcc.Loading(
             id="loading",
-            type="graph",
+            type="dot",
             fullscreen=True,
             color="#119DFF",
             style={"zIndex": "1000"},
