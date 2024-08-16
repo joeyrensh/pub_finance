c409cca645a79ae6a3071a79e29c8bfdb11f7839
joey <xiaoming_1862@163.com>
2024-08-16 12:29:23 +0800

    update

--- pub_finance/finance/test_sample.py
+++ pub_finance/finance/test_sample.py
@@ -27,22 +27,20 @@ logger = logging.getLogger(__name__)
 
 # https://www.useragents.me/#most-common-desktop-useragents-json-csv 获取最新user agent
 user_agent_list = [
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux i686; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:128.0) Gecko/20100101 Firefox/128.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/25.0 Chrome/121.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Unique/100.7.6266.6",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.",
     "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115."
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.3"
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 ]
 
 
@@ -52,11 +50,11 @@ def after_retry(retry_state):
 
 
 @retry(wait=wait_random(min=3, max=5), stop=stop_after_attempt(3), after=after_retry)
-def get_max_page(url, headers, proxies):
+def get_max_page(url, headers, proxy):
     time.sleep(random.randint(3, 5))  # 随机休眠
     s = requests.Session()
     s.headers.update(headers)
-    s.proxies = proxies
+    s.proxies = proxy
     response = s.get(url)
     print("response code:", response.status_code)
     # for cookie in response.headers.getlist("Set-Cookie"):
@@ -120,9 +118,14 @@ headers = {
     "Referer": "http://www.google.com",
     "Cookie": "lianjia_uuid=%s;" % (uuid.uuid4()),
 }
-proxies = {
-    "http": "http://133.18.234.13:80",
-    # "https": "http://101.126.44.74:8080",
-}
-max_page = get_max_page(url, headers, proxies)
+proxies = [
+    "http://183.234.215.11:8443",
+    "http://115.223.31.44:20002",
+    "http://111.225.153.113:8089",
+    "http://111.225.153.43:8089",
+    "http://111.225.152.201:8089",
+]
+proxy = {"http": random.choice(proxies)}
+print(proxy)
+max_page = get_max_page(url, headers, proxy)
 # check_proxy_anonymity(url, headers, proxies)
