5112f58b5a6b1ca44f91142a854a9727e7a53e45
ubuntu <ubuntu@localhost.localdomain>
2024-11-04 19:17:50 +0800

    update

--- pub_finance/finance/globalcategory_main.py
+++ pub_finance/finance/globalcategory_main.py
@@ -8,9 +8,9 @@ from utility.toolkit import ToolKit
 # 主程序入口
 if __name__ == "__main__":
     """爬取每日最新股票对应行业数据"""
-    # trade_date = ToolKit("获取最新美股交易日期").get_us_latest_trade_date(0)
-    # emi = EMUsTickerCategoryCrawler()
-    # emi.get_us_ticker_category(trade_date)
+    trade_date = ToolKit("获取最新美股交易日期").get_us_latest_trade_date(0)
+    emi = EMUsTickerCategoryCrawler()
+    emi.get_us_ticker_category(trade_date)
 
     """ 爬取每日最新股票对应行业数据 """
     trade_date_cn = ToolKit("获取最新A股交易日期").get_cn_latest_trade_date(0)
