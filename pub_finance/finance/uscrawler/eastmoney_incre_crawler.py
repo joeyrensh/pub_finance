d752e6867e40d27db5444069525500bcd807dfb0
Joey <xiaoming_1862@163.com>
2024-12-16 18:43:48 +0800

    update

--- pub_finance/finance/uscrawler/eastmoney_incre_crawler.py
+++ pub_finance/finance/uscrawler/eastmoney_incre_crawler.py
@@ -9,6 +9,7 @@ import pandas as pd
 from datetime import datetime
 import os
 from utility.fileinfo import FileInfo
+import csv
 
 
 class EMWebCrawler:
@@ -90,3 +91,35 @@ class EMWebCrawler:
         date = datetime.strptime(trade_date, "%Y%m%d")
         df["date"] = date
         df.to_csv(file_name_d, mode="w", index=True, header=True)
+
+        # 10年期国债收益率
+        url = "https://quote.eastmoney.com/center/api/qqzq.js?"
+        res = requests.get(url).text
+
+        lines = res.strip().split("\n")
+        data = []
+        for line in lines:
+            fields = line.split(",")
+            data.append(fields)
+
+        filtered_rows = []
+        for row in data[2:]:
+            if len(row) > 2:  # 确保parts列表至少有两个元素
+                code = row[1]
+                if code == "US10Y_B":
+                    code = row[1]
+                    name = row[2]
+                    date = row[3]
+                    new = row[5]
+
+                    # 将这些字段添加到一个新的列表中，准备写入CSV文件
+                    filtered_rows.append([code, name, date, new])
+
+        output_filename = FileInfo("trade_date", "us").get_file_path_gz
+        with open(output_filename, mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            # 可选：写入表头
+            writer.writerow(["code", "name", "date", "new"])
+            # 写入数据行
+            writer.writerows(filtered_rows)
