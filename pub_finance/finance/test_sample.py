598248a89145f32e587fca0493131958a007be0b
Joey <xiaoming_1862@163.com>
2025-03-13 09:44:21 +0800

    update

--- pub_finance/finance/test_sample.py
+++ pub_finance/finance/test_sample.py
@@ -353,7 +353,7 @@ if __name__ == "__main__":
     print("Garbage collector: collected %d objects." % (collected))
 
     """ 发送邮件 """
-    StockProposal("us", trade_date).send_btstrategy_by_email(6526093.9, 11977717.21)
+    StockProposal("us", trade_date).send_btstrategy_by_email(6850221.55, 11660249.14)
 
     """ 结束进度条 """
     pbar.finish()
--- pub_finance/finance/utility/toolkit.py
+++ pub_finance/finance/utility/toolkit.py
@@ -275,7 +275,7 @@ class ToolKit:
         img = BytesIO()
 
         # store the above plot to this Bytes object
-        fig.savefig(img, format="png", dpi=120)
+        fig.savefig(img, format="png", dpi=120, facecolor="none")
 
         # Encode object as base64 byte string
         encoded = b64encode(img.getvalue())
