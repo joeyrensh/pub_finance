af2d787085375df366a5009615c78f946b65acb2
Joey <xiaoming_1862@163.com>
2025-04-23 09:56:13 +0800

    update

--- pub_finance/finance/cnstock_main.py
+++ pub_finance/finance/cnstock_main.py
@@ -317,7 +317,7 @@ def exec_btstrategy(date):
             format="svg",
             bbox_inches="tight",  # 保持边界紧凑
             transparent=True,  # 保持背景透明
-            pad_inches=0.1,
+            pad_inches=0.2,
         )
         plt.close()
 
