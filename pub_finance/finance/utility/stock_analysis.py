e88d18f0712b513b616a40215ac18f4cf83dd4fc
joey.ren <xiaoming_1862@163.com>
2025-10-19 10:15:51 +0800

    update

--- pub_finance/finance/utility/stock_analysis.py
+++ pub_finance/finance/utility/stock_analysis.py
@@ -1657,7 +1657,8 @@ class StockProposal:
         pd_strategy_tracking_lst180days = spark_strategy_tracking_lst180days.toPandas()
         pd_strategy_tracking_lst180days["date"] = pd.to_datetime(
             pd_strategy_tracking_lst180days["date"]
-        )
+        ).dt.date
+        print("date:", pd_strategy_tracking_lst180days["date"])
         pd_strategy_tracking_lst180days["ema_success_rate"] = (
             pd_strategy_tracking_lst180days["success_rate"]
             .ewm(span=5, adjust=False)
