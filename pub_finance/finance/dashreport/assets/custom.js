f3b7a2de97d649f7de305aa4abfd6da7788ae129
joey.ren <xiaoming_1862@163.com>
2025-10-21 15:54:51 +0800

    update

--- pub_finance/finance/dashreport/assets/custom.js
+++ pub_finance/finance/dashreport/assets/custom.js
@@ -113,31 +113,13 @@
             }
 
             console.log(`Processing SVG: ${obj.id}`);
-            const textElements = svgDoc.getElementsByTagName('text');
-            const totalElements = textElements.length;
-            // 动态计算需要处理的元素数量
-            let maxElements = totalElements;
-            if (`${obj.id}` === 'cn-by-position-light' || `${obj.id}` === 'cn-by-position-dark' ||
-                `${obj.id}` === 'us-by-position-light' || `${obj.id}` === 'us-by-position-dark' ||
-                `${obj.id}` === 'us_special-by-position-light' || `${obj.id}` === 'us_special-by-position-dark' ||
-                `${obj.id}` === 'cn-by-pl-light' || `${obj.id}` === 'cn-by-pl-dark' ||
-                `${obj.id}` === 'us-by-pl-light' || `${obj.id}` === 'us-by-pl-dark' ||
-                `${obj.id}` === 'us_special-by-pl-light' || `${obj.id}` === 'us_special-by-pl-dark'
-            ) {
-                maxElements = totalElements * 0.4;
-            } else {
-                maxElements = totalElements;
-            }
-
-            let processedCount = 0;            
+            const textElements = svgDoc.getElementsByTagName('text');   
             for (let text of textElements) {
-                replaceFontSize(text, obj.id);
-                processedCount++;
-                
-                // 达到10个后停止循环
-                if (processedCount >= maxElements) {
-                    break;
-                }                
+                const transform = text.getAttribute('transform');
+                if (transform && transform.includes('scale')) {
+                    continue; // 跳过包含scale变换的元素
+                }
+                replaceFontSize(text, obj.id);          
             }
         } catch (e) {
             console.error('Error processing SVG:', e);
