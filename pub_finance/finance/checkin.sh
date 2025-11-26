00a74526538eb39e7d8ea1024007df211a9582e4
joey.ren <xiaoming_1862@163.com>
2025-11-26 14:56:22 +0800

    update

--- pub_finance/finance/checkin.sh
+++ pub_finance/finance/checkin.sh
@@ -1,3 +1,4 @@
+rm -rf ./git-repo/pub_finance/pub_finance/*
 \cp -rf ./pub_finance/finance ./git-repo/pub_finance/pub_finance/
 echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/pub_finance/finance/mail.conf
 cd ./git-repo/pub_finance/pub_finance/
