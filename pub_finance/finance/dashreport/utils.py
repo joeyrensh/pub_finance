ed6aa5c4f5b0923ecb6d36241f4d82d8ec88812c
joey.ren <xiaoming_1862@163.com>
2025-12-19 15:18:11 +0800

    update

--- pub_finance/finance/dashreport/utils.py
+++ pub_finance/finance/dashreport/utils.py
@@ -598,6 +598,18 @@ def make_dash_format_table(df, cols_format, market):
 
         condition = df["total_score"] > highlight_threshold
         df.loc[condition, "NAME"] = "3A+" + df.loc[condition, "NAME"]
+
+    def create_link(symbol, market):
+        if market == "cn" and symbol.startswith(("SH", "SZ")):
+            return f"[{symbol}](https://quote.eastmoney.com/{symbol}.html)"
+        elif market == "cn" and symbol.startswith("ETF"):
+            return f"[{symbol}](https://quote.eastmoney.com/{symbol[3:]}.html)"
+        else:
+            return f"[{symbol}](https://quote.eastmoney.com/us/{symbol}.html)"
+
+    if "SYMBOL" in df.columns:
+        df["SYMBOL"] = df["SYMBOL"].apply(lambda symbol: create_link(symbol, market))
+
     highlight_cols = [
         "IDX",
         "SYMBOL",
@@ -619,17 +631,6 @@ def make_dash_format_table(df, cols_format, market):
         if has_all_required_cols and "IND_ARROW_NUM" in df.columns
         else []
     )
-
-    def create_link(symbol, market):
-        if market == "cn" and symbol.startswith(("SH", "SZ")):
-            return f"[{symbol}](https://quote.eastmoney.com/{symbol}.html)"
-        elif market == "cn" and symbol.startswith("ETF"):
-            return f"[{symbol}](https://quote.eastmoney.com/{symbol[3:]}.html)"
-        else:
-            return f"[{symbol}](https://quote.eastmoney.com/us/{symbol}.html)"
-
-    if "SYMBOL" in df.columns:
-        df["SYMBOL"] = df["SYMBOL"].apply(lambda symbol: create_link(symbol, market))
 
     columns = [
         {
