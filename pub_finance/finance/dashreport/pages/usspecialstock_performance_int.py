1c7141ae0d9f25d77d6380fb993a385caec51e69
joey.ren <xiaoming_1862@163.com>
2026-02-05 09:55:34 +0800

    update

--- pub_finance/finance/dashreport/pages/usspecialstock_performance_int.py
+++ pub_finance/finance/dashreport/pages/usspecialstock_performance_int.py
@@ -412,7 +412,6 @@ def create_layout(app):
                                                             "padding": 0,
                                                             "width": "100%",
                                                             "height": "100%",
-                                                            "overflow": "visible",
                                                         },
                                                     )
                                                 ],
