a6d5e75a9150282b1784e7beaa5100eaff4e0bbb
joey.ren <xiaoming_1862@163.com>
2026-03-09 11:24:11 +0800

    update

--- pub_finance/finance/checkin.sh
+++ pub_finance/finance/checkin.sh
@@ -32,5 +32,6 @@ done
 echo -e "mail_name,mail_password\nmail_name1" > ./git-repo/pub_finance/pub_finance/finance/mail.conf
 cd ./git-repo/pub_finance/pub_finance/
 git add *
+git status
 git commit -a -m "update"
 git push
